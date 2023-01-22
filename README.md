fc25cbd02070a0e20e915bda07412413317ba991
Ilaria <petreti.ilaria@gmail.com>
2023-01-22 09:08:03 +0300

    Updated README and added indexer using an ingest pipeline

--- README.md
+++ README.md
@@ -4,13 +4,17 @@ Text Embeddings and Vector Search with Elasticsearch and Open-Source Technologie
 ## Pipeline:
 - Produce Vectors Externally
 
-`from_text_to_vectors`
+`from_text_to_vectors` &rarr; `batch-sentence-transformers.py`
 
 - Index documents to Elasticsearch
 
-`indexing_phase`
+`indexing_phase` &rarr; `indexer_elastic.py`
+
 
 ### Advance feature (Platinum or Enterprise License)
+- Index documents to Elasticsearch using a Text Embedding Ingest Pipeline
+
+`indexing_phase` &rarr; `indexer_elastic_with_pipeline.py`
 
 - Import and load language model to do inference directly within Elasticsearch
 
@@ -21,6 +25,7 @@ If you run `import_model.py` with basic license you got the following error:
 elasticsearch.AuthorizationException: AuthorizationException(403, 'security_exception', 'current license is non-compliant for [ml]')
 ````
 To use it, start a free trial: https://www.elastic.co/guide/en/elasticsearch/reference/current/start-trial.html
+
 
 ## Requirements:
 
---
+++ indexing_phase/indexer_elastic_with_pipeline.py
@@ -0,0 +1,59 @@
+import sys
+import time
+import random
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+BATCH_SIZE = 1000
+
+# Elastic configuration.
+ELASTIC_ADDRESS = "http://localhost:9200"
+INDEX_NAME = "neural_index"
+PIPELINE_NAME = "text-embeddings"
+
+def index_documents(documents_filename, client):
+    # Open the file containing text.
+    with open(documents_filename, "r") as documents_file:
+            documents = []
+            # For each document creates a JSON document including text (and id).
+            for index, document in enumerate(documents_file):
+                # Generate color value randomly (additional feature to show FILTER query behaviour).
+                color = random.choice(['red', 'green', 'white', 'black'])
+                # Create the JSON document including index name and pipeline.
+                doc = {
+                    "_index": INDEX_NAME,
+                    "pipeline": PIPELINE_NAME,
+                    "_id": str(index),
+                    "general_text": document,
+                    "color": color,
+                }
+                # Append JSON document to a list.
+                documents.append(doc)
+
+                # To index batches of documents at a time.
+                if index % BATCH_SIZE == 0 and index != 0:
+                    # How you'd index data to Elastic.
+                    indexing = bulk(client, documents)
+                    documents = []
+                    print("Success - %s , Failed - %s" % (indexing[0], len(indexing[1])))
+            # To index the rest, when 'documents' list < BATCH_SIZE.
+            if documents:
+                bulk(client, documents)
+            print("Finished")
+
+def main():
+    document_filename = sys.argv[1]
+
+    # Declare a client instance of the Python Elasticsearch library.
+    client = Elasticsearch(hosts=[ELASTIC_ADDRESS])
+    # Use this instead, IF using SECURITY ENABLED.
+    # client = Elasticsearch(hosts=[ELASTIC_ADDRESS], ca_certs=CA_CERTS_PATH, basic_auth=("elastic", ELASTIC_PASSWORD))
+
+    initial_time = time.time()
+    index_documents(document_filename, client)
+    finish_time = time.time()
+    print('Documents indexed in {:f} seconds\n'.format(finish_time - initial_time))
+
+
+if __name__ == "__main__":
+    main()
